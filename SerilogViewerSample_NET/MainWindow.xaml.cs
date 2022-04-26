afb66342d003647790f5ce87de9ed7258af1a10c
Tanzi <None>
2022-04-26 20:12:12 +0800

    (1)add SerilogViewer_NET (copied from SerilogViewer) (2)add SerilogViewerSample_NET (copied from SerilogViewerSample)

---
+++ SerilogViewerSample_NET/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using Serilog;
+using Serilog.Events;
+using SerilogViewer;
+using System;
+using System.Media;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SerilogViewerSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Task _logTask;
+        CancellationTokenSource _cancelLogTask;
+        ILogger log;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            cbAutoScroll.IsChecked = true;
+
+            log = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.SerilogViewerSink(logCtrl)
+                .CreateLogger();
+
+            logCtrl.ItemAdded += OnLogMessageItemAdded;
+
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            LogEventLevel level = LogEventLevel.Verbose;
+            if (sender.Equals(btnDebug)) level = LogEventLevel.Debug;
+            if (sender.Equals(btnWarning)) level = LogEventLevel.Warning;
+            if (sender.Equals(btnError)) level = LogEventLevel.Error;
+
+            if ((bool)cbWithContext.IsChecked)
+            {
+                log.ForContext<MainWindow>().Write(level, tbLogText.Text);
+            }
+            else
+            {
+                log.Write(level, tbLogText.Text);
+            }
+        }
+
+        private void SendWithContext_Click(object sender, RoutedEventArgs e)
+        {
+            LogEventLevel level = LogEventLevel.Verbose;
+            if (sender.Equals(btnDebug)) level = LogEventLevel.Debug;
+            if (sender.Equals(btnWarning)) level = LogEventLevel.Warning;
+            if (sender.Equals(btnError)) level = LogEventLevel.Error;
+
+            log.ForContext<MainWindow>().Write(level, tbLogText.Text);
+        }
+
+        private void OnLogMessageItemAdded(object o, EventArgs Args)
+        {
+            // Do what you want :)
+            LogEvent logEvent = (SerilogEvent)Args;
+            if (logEvent.Level >= LogEventLevel.Error)
+                SystemSounds.Beep.Play();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            logCtrl.Clear();
+        }
+        private void TopScroll_Click(object sender, RoutedEventArgs e)
+        {
+            logCtrl.ScrollToFirst();
+        }
+        private void BottomScroll_Click(object sender, RoutedEventArgs e)
+        {
+            logCtrl.ScrollToLast();
+        }
+        private void AutoScroll_Checked(object sender, RoutedEventArgs e)
+        {
+            logCtrl.AutoScrollToLast = true;
+        }
+        private void AutoScroll_Unchecked(object sender, RoutedEventArgs e)
+        {
+            logCtrl.AutoScrollToLast = false;
+        }
+
+
+        private void BackgroundSending_Checked(object sender, RoutedEventArgs e)
+        {
+            _cancelLogTask = new CancellationTokenSource();
+            var token = _cancelLogTask.Token;
+            _logTask = new Task(SendLogs, token, token);
+            _logTask.Start();
+        }
+
+        private void BackgroundSending_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (_cancelLogTask != null)
+                _cancelLogTask.Cancel();
+        }
+
+        private void SendLogs(object obj)
+        {
+            CancellationToken ct = (CancellationToken)obj;
+
+            int counter = 0;
+
+            var backgroundLogger = log.ForContext("Type", "Backgroundtask");
+
+            backgroundLogger.Debug("Backgroundtask started.");
+
+            while (!ct.WaitHandle.WaitOne(2000))
+            {
+                backgroundLogger.ForContext("Type", "Backgroundtask").Verbose(string.Format("Message number: {0} from backgroudtask.", counter++));
+            }
+
+            backgroundLogger.Debug("Backgroundtask stopped.");
+        }
+    }
+}
---
+++ SerilogViewer_NET/LogEventViewModel.cs
@@ -0,0 +1,65 @@
+﻿using Serilog.Events;
+using System;
+using System.Globalization;
+using System.Windows.Media;
+
+namespace SerilogViewer
+{
+    public class LogEventViewModel
+    {
+        private LogEvent logEvent;
+        private IFormatProvider formatProvider;
+
+        public string Time { get; private set; }
+        public string SourceContext { get; set; }
+        public string Level { get; private set; }
+        public string FormattedMessage { get; private set; }
+        public Exception Exception { get; private set; }
+        public string ToolTip { get; private set; }
+        public SolidColorBrush Background { get; private set; }
+        public SolidColorBrush Foreground { get; private set; }
+        public SolidColorBrush BackgroundMouseOver { get; private set; }
+        public SolidColorBrush ForegroundMouseOver { get; private set; }
+
+        public LogEventViewModel(LogEvent logEvent, IFormatProvider formatProvider)
+        {
+            // TODO: Complete member initialization
+            this.logEvent = logEvent;
+            this.formatProvider = formatProvider;
+
+            ToolTip = logEvent.RenderMessage();
+            Level = logEvent.Level.ToString();
+            FormattedMessage = logEvent.RenderMessage(formatProvider);
+            Exception = logEvent.Exception;
+            LogEventPropertyValue sourceContext;
+            if(logEvent.Properties.TryGetValue("SourceContext", out sourceContext))
+            {
+                SourceContext = sourceContext.ToString();
+            }
+            Time = logEvent.Timestamp.ToString("G", CultureInfo.CurrentCulture);
+
+            SetupColors(logEvent);
+        }
+
+        private void SetupColors(LogEvent logEvent)
+        {
+            if (logEvent.Level == LogEventLevel.Warning)
+            {
+                Background = Brushes.Yellow;
+                BackgroundMouseOver = Brushes.GreenYellow;
+            }
+            else if (logEvent.Level == LogEventLevel.Error)
+            {
+                Background = Brushes.Tomato;
+                BackgroundMouseOver = Brushes.IndianRed;
+            }
+            else
+            {
+                Background = Brushes.White;
+                BackgroundMouseOver = Brushes.LightGray;
+            }
+            Foreground = Brushes.Black;
+            ForegroundMouseOver = Brushes.Black;
+        }
+    }
+}
